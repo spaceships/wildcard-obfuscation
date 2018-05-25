e3e1e291f7d42c9204111f08b2918f884303fe8c
Brent Carmer <bcarmer@galois.com>
2018-05-25 13:20:25 -0700

    use better primes

--- benches/wildcard.rs
+++ benches/wildcard.rs
@@ -46,22 +46,5 @@ fn bench_eval_base(b: &mut Bencher, n: usize, secparam: usize) {
     });
 }
 
-// #[bench] fn obf_s80_n64 (b: &mut Bencher) { bench_obf_base(b, 64,  80) }
-// #[bench] fn obf_s80_n128(b: &mut Bencher) { bench_obf_base(b, 128, 80) }
-// #[bench] fn obf_s80_n256(b: &mut Bencher) { bench_obf_base(b, 256, 80) }
-// #[bench] fn obf_s80_n512(b: &mut Bencher) { bench_obf_base(b, 512, 80) }
-
-// #[bench] fn obf_s128_n64 (b: &mut Bencher) { bench_obf_base(b, 64,  128) }
-#[bench] fn obf_s128_n128(b: &mut Bencher) { bench_obf_base(b, 128, 128) }
-// #[bench] fn obf_s128_n256(b: &mut Bencher) { bench_obf_base(b, 256, 128) }
-// #[bench] fn obf_s128_n512(b: &mut Bencher) { bench_obf_base(b, 512, 128) }
-
-// #[bench] fn eval_s80_n64 (b: &mut Bencher) { bench_eval_base(b, 64,  80) }
-// #[bench] fn eval_s80_n128(b: &mut Bencher) { bench_eval_base(b, 128, 80) }
-// #[bench] fn eval_s80_n256(b: &mut Bencher) { bench_eval_base(b, 256, 80) }
-// #[bench] fn eval_s80_n512(b: &mut Bencher) { bench_eval_base(b, 512, 80) }
-
-// #[bench] fn eval_s128_n64 (b: &mut Bencher) { bench_eval_base(b, 64,  128) }
-#[bench] fn eval_s128_n128(b: &mut Bencher) { bench_eval_base(b, 128, 128) }
-// #[bench] fn eval_s128_n256(b: &mut Bencher) { bench_eval_base(b, 256, 128) }
-// #[bench] fn eval_s128_n512(b: &mut Bencher) { bench_eval_base(b, 512, 128) }
+#[bench] fn obf_s1024_n64 (b: &mut Bencher) { bench_obf_base(b, 64,  1024) }
+#[bench] fn eval_s1024_n64 (b: &mut Bencher) { bench_eval_base(b, 64,  1024) }
