5ef153cb475a44df3c75e43c474c7bf040f08ede
Brent Carmer <bcarmer@galois.com>
2018-06-22 14:24:18 -0700

    enhance tests, fix pattern combiner

--- scripts/linear_classifier.sh
+++ scripts/linear_classifier.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 RED='\033[0;31m'
+GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 ntests=0
 nbits=8
 verbose=""
+combine=""
 
 usage() {
     echo "$0 [options] [thresholds]"
     echo "    -t NUM    run NUM random tests"
     echo "    -h        this message"
     echo "    -b        number of bits [$nbits]"
+    echo "    -c        combine patterns"
     echo "    -v        verbose mode"
 }
 
@@ -40,6 +43,10 @@ while [[ $# -gt 0 ]]; do
             verbose=1
             shift
             ;;
+        -c)
+            combine=1
+            shift
+            ;;
         -h | --help)
             usage
             exit 0
@@ -63,31 +70,40 @@ if [[ $# -eq 0 ]]; then
 fi
 
 generate() {
-    IFS=$'\n'
-    for pat in $(./scripts/pattern_generator.hs -n $@); do
-        str=$(perl -E "\$a=$pat; for (@\$a) { printf q/%0${nbits}b/, \$_ }")
-        [[ $verbose ]] && echo $str >/dev/stderr
-        echo $str
-    done
-    unset IFS
+    command="./scripts/pattern_generator.hs -b $nbits $@"
+    if [[ $combine ]]; then 
+        command+=" -c"
+    fi
+    [[ $verbose ]] && echo "$command" >/dev/stderr
+    pats=$($command)
+    [[ $verbose ]] && echo "$pats" >/dev/stderr
+    echo "$pats"
 }
 
 # generate wildcard obfuscation
 generate $@ | cargo run --release -- multimatch -
 
 # run tests
+nfailed=0
 for ((i = 0; i < ntests; i++)); do 
     inpstr=""
     should_be=1
     for ((j = 0; j < $#; j++)); do
         inp=($(perl -e "print int(rand(1<<($nbits)))"))
         inpstr+=$(perl -e "printf q/%0${nbits}b/, $inp")
-        if [[ $inp -ge ${args[$j]} ]]; then
+        if [[ $inp -gt ${args[$j]} ]]; then
             should_be=0
         fi
     done
     res=$(cargo run --quiet --release -- eval $inpstr)
     if [[ $res -ne $should_be ]]; then
+        nfailed=$(($nfailed + 1))
         echo -e "${RED}test failed!${NC} input=$inpstr result=$res should_be=$should_be"
     fi
 done
+
+if [[ $ntests -gt 0 && $nfailed -eq 0 ]]; then
+    echo -e "${GREEN}passed $ntests tests!$NC"
+else 
+    echo -e "${RED}failed $nfailed tests${NC}"
+fi
--- scripts/pattern_generator.hs
+++ scripts/pattern_generator.hs
@@ -1,20 +1,25 @@
 #!/usr/bin/runghc
 
 import System.Environment
-import Text.Printf
 import Control.Monad hiding (join)
 import System.IO
 import System.Exit
 import Data.Char
 import Data.List
 import Data.Ord
+import Data.Bits
 
 main = do
-    args <- getArgs
-    let strs = case head args of
-            "-n" -> map show $ generate (tail (map read args))
-            _    -> genStrs (map read args)
-    mapM putStrLn strs
+    (nbits, comb, args) <- processArgs (8, False, []) <$> getArgs
+    let strs = genStrs nbits (map read args)
+    mapM putStrLn (if comb then combine strs else strs)
+
+processArgs :: (Int, Bool, [String]) -> [String] -> (Int, Bool, [String])
+processArgs t [] = t
+processArgs t@(nbits, combineFlag, positional) args = case head args of
+    "-c" -> processArgs (nbits, True, positional) (tail args)
+    "-b" -> processArgs (read (head (tail args)), combineFlag, positional) (tail (tail args))
+    arg  -> processArgs (nbits, combineFlag, positional ++ [arg]) (tail args)
 
 sortByStars :: [String] -> [String]
 sortByStars = sortBy starsCmp
@@ -25,13 +30,16 @@ sortByStars = sortBy starsCmp
 generate :: [Int] -> [[Int]]
 generate thresholds = sequence [ [ 0..max ] | max <- thresholds ]
 
-genStrs :: [Int] -> [String]
-genStrs thresholds = map (concat . map (printf "%08b")) (generate thresholds)
+genStrs :: Int -> [Int] -> [String]
+genStrs nbits thresholds = map (concat . map (asBinary nbits)) (generate thresholds)
 
 -- hamming distance
 delta :: String -> String -> Int
 delta s1 s2 = sum (zipWith ham s1 s2)
   where
+    ham '*' '*' = 0
+    ham '*' _ = 1
+    ham _ '*' = 1
     ham c1 c2 = abs (ord c1 - ord c2)
 
 -- put a star where the strings differ
@@ -46,6 +54,12 @@ combine xs = case combineRec xs 0 of
 
 combineRec :: [String] -> Int -> ([String], Int)
 combineRec [] n = ([], n)
-combineRec (x:xs) n = case find ((==) 1 . delta x) xs of
+combineRec (x:xs) n = case find (\y -> delta x y <= 1) xs of
     Just y  -> combineRec (star x y : delete y xs) (n+1)
     Nothing -> let (ys,n') = combineRec xs n in (x:ys, n')
+
+asBinary :: Int -> Int -> String
+asBinary nbits x = reverse (helper nbits x)
+  where
+    helper 0 _ = ""
+    helper n y = let b = if testBit y 0 then '1' else '0' in b : helper (n-1) (shiftR y 1)
